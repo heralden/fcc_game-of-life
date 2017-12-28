8ce62e12e4efb103b2838fd41d90ad56b913dd27
Kevin <regen@anche.no>
2017-12-28 23:54:15 +0100

    Sketch App component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cells: 0
+    };
+  }
+
+  componentWillMount() {
+    this.updateCells();
+  }
+  componentDidMount() {
+    window.addEventListener('resize', this.updateCells);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateCells);
+  }
+
+  updateCells = () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    this.setState({
+      cells: Math.floor((height * width) / 144)
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        {Array(this.state.cells).fill('').map((e, i) => (
+          <div className="App-cell"
+            key={i}
+          />
+        ))}
+
       </div>
     );
   }
