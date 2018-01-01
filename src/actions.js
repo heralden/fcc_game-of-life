f03e0ca5c5f9c5284d0a223060d034e5abc6ab39
Kevin <regen@anche.no>
2018-01-01 17:46:43 +0100

    Add neighboringCells function with test

--- src/actions.js
+++ src/actions.js
@@ -14,6 +14,45 @@ export function setCell(cells, y, x, val) {
   return newCells;
 }
 
+export function neighboringCells(cells, y, x) {
+  const width = cells[0].length;
+  const height = cells.length;
+
+  const north = { y: y - 1, x }
+      , east = { y, x: x + 1 }
+      , south = { y: y + 1, x }
+      , west = { y, x: x - 1 };
+
+  const neighbors = [
+    north, east, south, west
+  ];
+
+  for (let dir in neighbors) {
+    // Fix overflowing coordinates
+    const neighbor = neighbors[dir];
+
+    if (neighbor.x >= width) 
+      neighbor.x -= width;
+    else if (neighbor.x < 0)
+      neighbor.x += width;
+
+    if (neighbor.y >= height)
+      neighbor.y -= height;
+    else if (neighbor.y < 0)
+      neighbor.y += height;
+  }
+
+  const northEast = { y: north.y, x: east.x }
+      , southEast = { y: south.y, x: east.x }
+      , southWest = { y: south.y, x: west.x }
+      , northWest = { y: north.y, x: west.x };
+
+  return [
+    north, northEast, east, southEast,
+    south, southWest, west, northWest
+  ];
+}
+
 export function updateCells(cells) {
   // WIP
   return cells;
--- src/actions.test.js
+++ src/actions.test.js
@@ -1,7 +1,8 @@
 import {
   cellArray,
   copyCells,
-  setCell
+  setCell,
+  neighboringCells
 } from './actions';
 
 it('returns an empty cell array of specified size', () => {
@@ -21,4 +22,20 @@ it('returns a cell array with the value of a cell changed', () => {
   targetCells[0][0] = true;
 
   expect(newCells).toEqual(targetCells);
+});
+
+it('returns an array of all 8 neighboring cell coordinates', () => {
+  const cells = cellArray(6, 4);
+  const neighbors = neighboringCells(cells, 0, 0);
+
+  expect(neighbors).toEqual([
+    { y: 3, x: 0 },
+    { y: 3, x: 1 },
+    { y: 0, x: 1 },
+    { y: 1, x: 1 },
+    { y: 1, x: 0 },
+    { y: 1, x: 5 },
+    { y: 0, x: 5 },
+    { y: 3, x: 5 }
+  ]);
 });
